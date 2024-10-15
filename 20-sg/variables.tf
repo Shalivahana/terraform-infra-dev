4e729fa2b697aa74901d5b4071ad1c7e8c7c5f09
Chary.manu <chary.manu78@gmail.com>
2024-10-15 17:58:29 +0530

    terraform

---
+++ 20-sg/variables.tf
@@ -0,0 +1,48 @@
+variable "project_name"{
+    default = "expense"
+}
+
+variable "environment"{
+    default = "dev"
+}
+
+variable "common_tags" {
+    default =  {
+        project_name = "expense"
+        environment = "dev"
+        terraform = "true"
+
+    }
+}
+
+
+variable "mysql_sg_tags" {
+    default= {
+        Component = "mysql"
+    }
+}
+
+
+variable "backend_sg_tags" {
+    default= {
+        Component = "backend"
+    }
+}
+
+variable "frontend_sg_tags" {
+    default= {
+        Component = "frontend"
+    }
+}
+
+variable "bastion_sg_tags" {
+    default= {
+        Component = "bastion"
+    }
+}
+
+variable "ansible_sg_tags" {
+    default= {
+        Component = "ansible"
+    }
+}
